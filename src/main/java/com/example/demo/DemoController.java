e57d2ad5be16405708765b42d30444f056a3503d
phamthanh94 <phamthanh.ptit@gmail.com>
2023-03-10 22:53:24 +0700

    Update DemoController.java

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DemoController {
 
     @GetMapping("/")
